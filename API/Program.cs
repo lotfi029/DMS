8f9d8e0c6a41ce8c36fdf98532046af15b783e95
Mohamed lotfi <mohamed80f@gmail.com>
2026-04-06 23:13:18 +0200

    Add project files.

---
+++ API/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.Run();
